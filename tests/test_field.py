05b44af8ddd7b63508b7d7c0975c58b222d8503a
Anton Shvein aka T0ha <t0hashvein@gmail.com>
2022-04-19 15:16:54 +0500

    Fixed tests after refactoring

--- tests/test_field.py
+++ tests/test_field.py
@@ -28,7 +28,7 @@ class TestField(unittest.TestCase):
     
     def test_encode_bool(self):
         field = Field('test', 'Bool', 1)
-        assert b'\x08\x00' == field.encode(False)
+        assert b'' == field.encode(False)
         assert b'\x08\x01' == field.encode(True)
 
     def test_encode_int32(self):
@@ -88,43 +88,43 @@ class TestField(unittest.TestCase):
         assert b'\x08' + ENCODED_65536 == field.encode(65536)
 
     def test_decode_encode_bool(self):
-        field = Field('test', 'Bool', 1)
-        assert (False, b'') == field.decode(field.encode(False))
-        assert (True, b'') == field.decode(field.encode(True))
+        field = Field('test', 'Bool', 1, default=4) # default is added to make it decode False
+        assert (False, b'') == field.decode(0x08, field.encode(False)[1:])
+        assert (True, b'') == field.decode(0x08, field.encode(True)[1:])
 
     def test_decode_encode_int32(self):
         numbers = [randint(1, 65536) for _ in range(1, randint(10, 30))]
         field = Field('test', 'Int32', 1)
 
         for n in numbers:
-            assert (n, b'') == field.decode(field.encode(n))
+            assert (n, b'') == field.decode(0x08, field.encode(n)[1:])
 
     def test_decode_encode_uint32(self):
         numbers = [randint(1, 65536) for _ in range(1, randint(10, 30))]
         field = Field('test', 'UInt32', 1)
 
         for n in numbers:
-            assert (n, b'') == field.decode(field.encode(n))
+            assert (n, b'') == field.decode(0x08, field.encode(n)[1:])
 
     def test_decode_encode_sint32(self):
         numbers = [randint(1, 65536) for _ in range(1, randint(10, 30))]
         field = Field('test', 'Int32', 1)
 
         for n in numbers:
-            assert (n, b'') == field.decode(field.encode(n))
+            assert (n, b'') == field.decode(0x08, field.encode(n)[1:])
 
     def test_decode_encode_int64(self):
         numbers = [randint(1, 65536) for _ in range(1, randint(10, 30))]
         field = Field('test', 'Int64', 1)
 
         for n in numbers:
-            assert (n, b'') == field.decode(field.encode(n))
+            assert (n, b'') == field.decode(0x08, field.encode(n)[1:])
     def test_decode_encode_uint64(self):
         numbers = [randint(1, 65536) for _ in range(1, randint(10, 30))]
         field = Field('test', 'UInt64', 1)
 
         for n in numbers:
-            assert (n, b'') == field.decode(field.encode(n))
+            self.assertEqual((n, b''), field.decode(0x08, field.encode(n)[1:]))
 
     @unittest.skip('Needs test data')
     def test_decode_encode_sint64(self):
@@ -132,12 +132,16 @@ class TestField(unittest.TestCase):
         field = Field('test', 'SInt64', 1)
 
         for n in numbers:
-            assert (n, b'') == field.decode(field.encode(n))
+            assert (n, b'') == field.decode(0x08, field.encode(n)[1:])
 
     def test_decode_encode_string(self):
         field = Field('test', 'String', 1)
-        assert ('test string', b'') == field.decode(field.encode('test string'))
+        data = field.encode('test string')
+        (tag, data) = field.get_tag(data)
+        assert ('test string', b'') == field.decode(tag, data)
 
     def test_decode_encode_bytes(self):
         field = Field('test', 'Bytes', 1)
-        assert (b'test string', b'') == field.decode(field.encode(b'test string'))
+        data = field.encode(b'test string')
+        (tag, data) = field.get_tag(data)
+        assert (b'test string', b'') == field.decode(tag, data)
